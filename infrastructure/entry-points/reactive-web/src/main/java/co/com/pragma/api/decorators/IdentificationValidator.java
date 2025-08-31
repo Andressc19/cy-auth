36299af45d0434e7c7f1cb5fc463412d71aaf8e8
Andressc19 <joseandrescram1005@gmail.com>
2025-08-31 01:49:30 -0500

    feat: update identification number validation to allow 1-15 digits and handle null values

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/decorators/IdentificationValidator.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/decorators/IdentificationValidator.java
@@ -6,10 +6,10 @@ import jakarta.validation.ConstraintValidatorContext;
 public class IdentificationValidator implements ConstraintValidator<ValidIdentificationNumber, String> {
 	
 	@Override
-	public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
-		if (!value.isEmpty()){
+	public boolean isValid(String value, ConstraintValidatorContext context) {
+		if (value == null || value.isEmpty()) {
 			return false;
 		}
-		return value.matches("^[0-9]{10}$");
+		return value.matches("^[0-9]{1,15}$");
 	}
 }
